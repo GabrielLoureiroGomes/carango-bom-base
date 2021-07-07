8b6e5e3b25a1d197f8f12df379311ce6d9775abb
Viviane Dias <vivipirolad@gmail.com>
2021-07-07 16:36:20 -0300

    feat(navbar): update name entrar -> login

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import { Paper, MenuItem, MenuList } from "@material-ui/core/";
 import { useStyles } from "./styles";
 
 export const publicLinks = [
-  { label: "Entrar", to: "/" },
+  { label: "Login", to: "/" },
   { label: "Veículos", to: "/veiculos" },
 ];
 
@@ -13,7 +13,7 @@ export const loggedLinks = [
   { label: "Marcas", to: "/marcas" },
   { label: "Usuários", to: "/usuarios" },
   { label: "Dashboard", to: "/dashboard" },
-  { label: "Sair", to: "#" },
+  { label: "Sair", to: "/" },
 ];
 
 function NavBar({ userLogged }) {
