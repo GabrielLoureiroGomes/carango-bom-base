6d522288fa1e098e816d528012cf5a369b69e977
Viviane Dias <vivipirolad@gmail.com>
2021-07-08 17:54:43 -0300

    feat(auth): reduce auth action to only one func

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,25 +1,14 @@
-import UserService from "../services/UserService";
 import { removeStorageToken, setStorageToken } from "../utils/auth";
+import UserService from "../services/UserService";
 
-export async function auth({ dispatch, user }) {
+export async function auth({ dispatch, user, method }) {
   // TODO: alinhar qual será a resposta com o back-end
   try {
-    const token = await UserService.auth(user);
+    const service = UserService[method];
+    if (!service) throw new Error("Unkown UserService method");
+    const token = await service(user);
     dispatch({ type: "auth", payload: user });
     setStorageToken(token);
-  } catch (e) {
-    dispatch({ type: "logout" });
-    removeStorageToken();
-    return e;
-  }
-}
-
-export async function signup({ dispatch, user }) {
-  // TODO: alinhar qual será a resposta com o back-end
-  try {
-    const { data } = await UserService.signup(user);
-    dispatch({ type: "auth", payload: user });
-    setStorageToken(data);
   } catch (e) {
     dispatch({ type: "logout" });
     removeStorageToken();
