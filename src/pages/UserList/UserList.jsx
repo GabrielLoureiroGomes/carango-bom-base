4d208c585d6ac2b83601ff1fed7891d03ac4c316
Wagner Lopes Menezes Filho <wagner.lopes@MacBook-Pro-de-Wagner.local>
2021-07-08 18:35:03 -0300

    refactor(userlist): use refactored table

--- src/pages/UserList/UserList.jsx
+++ src/pages/UserList/UserList.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { useHistory } from "react-router";
+import React from "react";
 
 import UserService from "../../services/UserService";
 
@@ -8,47 +7,7 @@ import { Table } from "../../components";
 const columns = [{ flex: 1, field: "name", headerName: "Nome" }];
 
 function UserList() {
-  const history = useHistory();
-
-  const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState();
-
-  const loadUsers = useCallback(async () => {
-    try {
-      const users = await UserService.getAll();
-      return setUsers(users);
-    } catch (e) {
-      console.log(e);
-    }
-  }, [setUsers]);
-
-  useEffect(loadUsers, [loadUsers]);
-
-  function updateUser() {
-    history.push("/usuario/" + selectedUser.id);
-  }
-
-  async function deleteUser() {
-    try {
-      await UserService.delete(selectedUser);
-      setUsers(users.filter((user) => user.id !== selectedUser.id));
-      setSelectedUser(null);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  return (
-    <Table
-      rowsContent={users}
-      columns={columns}
-      setSelectedItem={setSelectedUser}
-      selectedItem={selectedUser}
-      addItem={() => history.push("/usuario/cadastro")}
-      updateItem={updateUser}
-      deleteItem={deleteUser}
-    />
-  );
+  return <Table service={UserService} route="usuario" columns={columns} />;
 }
 
 export default UserList;
