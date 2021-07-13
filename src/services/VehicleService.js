50a93313fa7494ab04bdd1daad9d708f83d95ff1
Viviane Dias <vivipirolad@gmail.com>
2021-07-13 09:23:52 -0300

    feat(vehicle): adds services needed by register page

--- src/services/VehicleService.js
+++ src/services/VehicleService.js
@@ -4,18 +4,18 @@ const VehicleService = {
       {
         id: 1,
         brand: {
-          id: 1,
-          name: "Fiat",
+          id: 34,
+          name: "FORD",
         },
-        model: "Uno",
+        model: "Ka",
         year: 2010,
         price: 15000,
       },
       {
         id: 2,
         brand: {
-          id: 2,
-          name: "Chevrolet",
+          id: 74,
+          name: "CHEVROLET",
         },
         model: "Corsa",
         year: 2015,
@@ -24,8 +24,56 @@ const VehicleService = {
     ]);
   },
 
-  update() {
-    return Promise.resolve();
+  get(id) {
+    return {
+      data: {
+        id: 1,
+        brand: {
+          id: 34,
+          nome: "FORD",
+        },
+        model: "Ka",
+        year: 2010,
+        price: 15000,
+      },
+    };
+  },
+
+  register(vehicle) {
+    return new Promise((resolve, reject) => {
+      if (
+        "model" in vehicle &&
+        "year" in vehicle &&
+        "price" in vehicle &&
+        "brandId" in vehicle
+      ) {
+        resolve();
+      } else {
+        reject({
+          status: 404,
+          data: "Marca não existe",
+        });
+      }
+    });
+  },
+
+  update(vehicle) {
+    return new Promise((resolve, reject) => {
+      if (
+        "model" in vehicle &&
+        "year" in vehicle &&
+        "price" in vehicle &&
+        "brandId" in vehicle &&
+        "id" in vehicle
+      ) {
+        resolve();
+      } else {
+        reject({
+          status: 404,
+          data: "Veículo não existe",
+        });
+      }
+    });
   },
 
   delete() {
