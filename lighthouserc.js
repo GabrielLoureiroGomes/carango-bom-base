d1ba36e09412f97cd10faaa3b44f22a2d95eca1f
Wagner Lopes Menezes Filho <wagner.lopes@MacBook-Pro-de-Wagner.local>
2021-07-20 15:35:19 -0300

    chore(lighthouse): add lighthouse ci config

---
+++ lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: "./build",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:performance": ["warn", { minScore: 0.9 }],
+      },
+    },
+  },
+};
