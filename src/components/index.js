59c3e676d59c0044191af69254eb2855c427d572
Viviane Dias <vivipirolad@gmail.com>
2021-07-22 18:43:46 -0300

    feat(auth): creates auth comp to abstract auth logic

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Table } from "./Table/Table";
 export { default as Select } from "./Select/Select";
 export { default as BarGraph } from "./BarGraph/BarGraph";
 export { default as Register } from "./Register/Register";
+export { default as Auth } from "./Auth/Auth";
