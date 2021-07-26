d4674c2ad53248389032d7c52427a5285b05f3b6
Viviane Dias <vivipirolad@gmail.com>
2021-07-26 13:04:13 -0300

    test(login): adds cypress tests to login

---
+++ cypress/integration/1-getting-started/login.spec.js
@@ -0,0 +1,31 @@
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/login");
+  });
+
+  it("displays username input", () => {
+    cy.get("#username").should("exist");
+  });
+  it("displays password input", () => {
+    cy.get("#password").should("exist");
+  });
+
+  // it("redirects to '/' after login", () => {
+  //   cy.login(`teste-${Math.random()}`, "teste123");
+  //   cy.location().should((location) => {
+  //     expect(location.pathname).to.eq("/");
+  //   });
+  // });
+
+  // it("set auth token in localStorage after login", () => {
+  //   cy.login(`teste-${Math.random()}`, "teste123");
+  //   cy.getLocalStorage(item).not.to.be.null;
+  // });
+
+  it("redirects to /cadastrar on click in 'cadastrar' button", () => {
+    cy.contains("Cadastrar").click();
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq("/cadastro");
+    });
+  });
+});
